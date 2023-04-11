8b8524a738128dcdaafb5cb95a5968076c075a29
tomu4 <tomu4@email.cz>
2023-04-11 17:59:14 +0200

    login button fix

--- hooks/loginbody.php
+++ hooks/loginbody.php
@@ -3,6 +3,6 @@ if(count(get_included_files()) ==1) die(); //Direct Access Not Permitted
 require_once '../users/init.php';
 global $settings, $user, $db, $authUrl, $us_url_root, $abS_us_root;
 if($settings->plg_sez_sezlogin==1 && !$user->isLoggedIn()){
-    require_once $abs_us_root.$us_url_root.'usersc/plugins/seznam_login/assets/seznam_oauth.php';
+    require_once $abs_us_root.$us_url_root.'usersc/plugins/seznam_login/assets/seznam_oauth_login.php';
   }
 ?>
